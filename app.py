b225b79108644d968becce8564ffcd1ef216e97c
PFA23SCM59R <prangineni@hawk.iit.edu>
2023-11-21 17:43:13 -0600

    statsmodel

--- app.py
+++ app.py
@@ -1,75 +1,128 @@
-# Import necessary packages
-from flask import Flask, jsonify, request
+# Import all the required packages
+from flask import Flask, jsonify, request, make_response
 import os
+from dateutil import *
+from datetime import timedelta
 import pandas as pd
-from fbprophet import Prophet
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from flask_cors import CORS
+
+# Statsmodels ARIMA related packages
+from statsmodels.tsa.arima.model import ARIMA
 
 # Import required storage package from Google Cloud Storage
 from google.cloud import storage
 
-# Initialize flask app
+# Initilize flask app
 app = Flask(__name__)
 # Handles CORS (cross-origin resource sharing)
 CORS(app)
-# Initialize Google Cloud Storage client
+# Initlize Google cloud storage client
 client = storage.Client()
 
-# API route path is "/api/forecast"
-# This API will accept only POST requests
+# Add response headers to accept all types of requests
+def build_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    response.headers.add("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
+    return response
+
+# Modify response headers when returning to the origin
+def build_actual_response(response):
+    response.headers.set("Access-Control-Allow-Origin", "*")
+    response.headers.set("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
+    return response
+
+'''
+API route path is "/api/forecast"
+This API will accept only POST request
+'''
 @app.route('/api/forecast', methods=['POST'])
 def forecast():
-    # Extract data from the request
     body = request.get_json()
     issues = body["issues"]
     type = body["type"]
     repo_name = body["repo"]
-
-    # Create a DataFrame from the GitHub data
     data_frame = pd.DataFrame(issues)
     df1 = data_frame.groupby([type], as_index=False).count()
     df = df1[[type, 'issue_number']]
     df.columns = ['ds', 'y']
 
-    # Rename the columns as per Prophet's requirement
-    df = df.rename(columns={'ds': 'ds', 'y': 'y'})
+    df['ds'] = df['ds'].astype('datetime64[ns]')
+    array = df.to_numpy()
+    x = np.array([time.mktime(i[0].timetuple()) for i in array])
+    y = np.array([i[1] for i in array])
 
-    # Initialize Prophet model
-    model = Prophet()
+    lzip = lambda *x: list(zip(*x))
 
-    # Fit the model with the GitHub data
-    model.fit(df)
+    days = df.groupby('ds')['ds'].value_counts()
+    Y = df['y'].values
+    X = lzip(*days.index.values)[0]
+    firstDay = min(X)
 
-    # Make a DataFrame with future dates for forecasting
-    future = model.make_future_dataframe(periods=365)  # Forecasting for 1 year (365 days)
+    '''
+    To achieve data consistency with both actual data and predicted values, 
+    add zeros to dates that do not have orders
+    [firstDay + timedelta(days=day) for day in range((max(X) - firstDay).days + 1)]
+    '''
+    Ys = [0, ]*((max(X) - firstDay).days + 1)
+    days = pd.Series([firstDay + timedelta(days=i) for i in range(len(Ys))])
+    for x, y in zip(X, Y):
+        Ys[(x - firstDay).days] = y
 
-    # Generate the forecast
-    forecast = model.predict(future)
+    # Modify the data that is suitable for ARIMA
+    Ys = np.array(Ys)
+    Ys = Ys.astype('float32')
 
-    # Plot the forecast components
-    fig = model.plot_components(forecast)
+    # Fit the ARIMA model with training data
+    arima_model = ARIMA(Ys, order=(5, 1, 0))  # You can adjust the order as needed
+    arima_results = arima_model.fit()
 
-    # Save the forecast plot as an image
-    forecast_plot_path = f"static/images/forecast_{type}_{repo_name}.png"
-    plt.savefig(forecast_plot_path)
+    # Predict issues for test data using ARIMA
+    y_pred_arima = arima_results.forecast(steps=len(Ys))[0]
 
-    # Add your unique Bucket Name if you want to run it locally
-    BUCKET_NAME = os.environ.get('BUCKET_NAME', 'Your_BUCKET_NAME')
+    # ... (rest of the existing code)
 
-    # Upload the forecast plot image to Google Cloud Storage
+    # Plot the ARIMA Generated image
+    fig, axs = plt.subplots(1, 1, figsize=(10, 4))
+    X = mdates.date2num(days)
+    axs.plot(np.arange(0, len(Y_train)), Y_train, 'g', label="history")
+    axs.plot(np.arange(len(Y_train), len(Y_train) + len(Y_test)),
+             Y_test, marker='.', label="true")
+    axs.plot(np.arange(len(Y_train), len(Y_train) + len(Y_test)),
+             y_pred_arima, 'r', label="prediction")
+    axs.legend()
+    axs.set_title('ARIMA Generated Data For ' + type)
+    axs.set_xlabel('Time Steps')
+    axs.set_ylabel('Issues')
+    # Save the figure in /static/images folder
+    plt.savefig(LOCAL_IMAGE_PATH + ALL_ISSUES_DATA_IMAGE_NAME)
+
+    # Uploads an images into the google cloud storage bucket
     bucket = client.get_bucket(BUCKET_NAME)
-    new_blob = bucket.blob(forecast_plot_path)
-    new_blob.upload_from_filename(filename=forecast_plot_path)
+    new_blob = bucket.blob(MODEL_LOSS_IMAGE_NAME)
+    new_blob.upload_from_filename(
+        filename=LOCAL_IMAGE_PATH + MODEL_LOSS_IMAGE_NAME)
+    new_blob = bucket.blob(ALL_ISSUES_DATA_IMAGE_NAME)
+    new_blob.upload_from_filename(
+        filename=LOCAL_IMAGE_PATH + ALL_ISSUES_DATA_IMAGE_NAME)
+    new_blob = bucket.blob(LSTM_GENERATED_IMAGE_NAME)
+    new_blob.upload_from_filename(
+        filename=LOCAL_IMAGE_PATH + LSTM_GENERATED_IMAGE_NAME)
 
-    # Construct the response with the image URL
+    # Construct the response
     json_response = {
-        "forecast_plot_image_url": f"{BUCKET_NAME}/{forecast_plot_path}"
+        "model_loss_image_url": MODEL_LOSS_URL,
+        "lstm_generated_image_url": LSTM_GENERATED_URL,
+        "all_issues_data_image": ALL_ISSUES_DATA_URL
     }
-
-    # Return the response to the Flask microservice
+    # Returns image url back to flask microservice
     return jsonify(json_response)
 
-# Run the app server on port 8080
+
+# Run LSTM app server on port 8080
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
